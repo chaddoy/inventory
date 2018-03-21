383dc8863fec26b4648758d1e4d0fa08eb079dfa
Chad Dumadag <capdumadag@gmail.com>
2018-03-21 17:00:32 +0800

    increase test coverage

--- app/containers/PrivateLayout/messages.js
+++ app/containers/PrivateLayout/messages.js
@@ -1,17 +1,17 @@
 /*
- * AdminLayout Messages
+ * PrivateLayout Messages
  *
- * This contains all the text for the AdminLayout component.
+ * This contains all the text for the PrivateLayout component.
  */
 import { defineMessages } from 'react-intl';
 
 export default defineMessages({
   header: {
-    id: 'app.containers.AdminLayout.header',
-    defaultMessage: 'This is AdminLayout container !',
+    id: 'app.containers.PrivateLayout.header',
+    defaultMessage: 'This is PrivateLayout container !',
   },
   footer: {
-    id: 'app.containers.AdminLayout.footer',
+    id: 'app.containers.PrivateLayout.footer',
     defaultMessage: 'Ant Design ©2016 Created by Ant UED',
   },
 });
